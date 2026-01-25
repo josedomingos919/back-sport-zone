459495f002766e60fb5e035e819adc7a1197dd78
josedomingos919 <josedomingos919@gmail.com>
2026-01-25 23:40:42 +0100

    feat: rest back end

--- src/province/province.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProvinceController } from './province.controller';
-import { ProvinceService } from './province.service';
-
-@Module({
-  controllers: [ProvinceController],
-  providers: [ProvinceService],
-})
-export class ProvinceModule {}
