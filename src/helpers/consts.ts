c4870cd9aa4d6aa5476e2e235aa99b8c217f8bd9
josedomingos919 <josedomingos919@gmail.com>
2026-01-26 01:29:25 +0100

    feat: save schema

--- src/helpers/consts.ts
+++ src/helpers/consts.ts
@@ -13,3 +13,23 @@ export const UserAccessTypeData = [
   { key: 'ATLETA', value: 'Atleta' },
   { key: 'VISITANTE', value: 'Visitante' },
 ];
+
+export const categoriaEquipeData = [
+  { key: 'FORMACAO', value: 'Formação' },
+  { key: 'SENIOR', value: 'Sénior' },
+  { key: 'FEMININO', value: 'Feminino' },
+  { key: 'MASCULINO', value: 'Masculino' },
+  { key: 'AMADOR', value: 'Amador' },
+  { key: 'PROFISSIONAL', value: 'Profissional' },
+];
+
+export const escaloesData = [
+  { key: 'SUB_7', value: 'Sub-7' },
+  { key: 'SUB_9', value: 'Sub-9' },
+  { key: 'SUB_11', value: 'Sub-11' },
+  { key: 'SUB_13', value: 'Sub-13' },
+  { key: 'SUB_15', value: 'Sub-15' },
+  { key: 'SUB_17', value: 'Sub-17' },
+  { key: 'SUB_19', value: 'Sub-19' },
+  { key: 'SENIOR', value: 'Sénior' },
+];
